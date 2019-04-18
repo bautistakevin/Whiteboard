80f26b4bb9871c3dca3f12804a79b82fd822f39f
Kevin Bautista <kevin.m.bautista@outlook.com>
2019-04-17 20:35:09 -0400

    added duplicateremoval project

---
+++ DuplicateStrings/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace DuplicateStrings
+{
+    //Remove all duplicate characters in a given string
+    //input = "Microsoft"
+    //output = "Micrsft"
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "Microsoft";
+            string result = DuplicateRemoval(input);
+            Console.WriteLine($"input = {input}, output = {result}");
+
+        }
+
+        static string DuplicateRemoval(string input)
+        {
+            string output = "";
+            for (int i = 0; i < input.Length; i++)
+            {
+                int counter = 0;
+                for (int j = 0; j < input.Length; j++)
+                {
+                   if(i==j)
+                    {
+                        j++;
+                    }
+                   else if(input[i]==input[j])
+                    {
+                        counter++;
+                    }                  
+                }
+                if (counter == 0)
+                {
+                    output += input[i];
+                }
+
+            }
+            return output;
+        }
+    }
+}
---
+++ MicrosoftInterviewQuestion/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MicrosoftInterviewQuestion
+{
+    /*Given an integer x. The task is 
+     * to find the square root of x. If x is not a perfect square, 
+     * then return floor(√x).*/
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
